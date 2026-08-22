5667b2bd3a065dde3cacf379096ba3ac86d01445
Hifu <artix1995@gmail.com>
2026-08-23 00:04:41 +0200

    Declare HDL stream IOP imports

---
+++ iop/hdl_stream/irx_imports.h
@@ -0,0 +1,9 @@
+#ifndef PS2_HDD_BOOTSTRAP_MANAGER_HDL_STREAM_IRX_IMPORTS_H
+#define PS2_HDD_BOOTSTRAP_MANAGER_HDL_STREAM_IRX_IMPORTS_H
+
+#include <irx.h>
+#include <iomanX.h>
+#include <sysclib.h>
+#include <sysmem.h>
+
+#endif
