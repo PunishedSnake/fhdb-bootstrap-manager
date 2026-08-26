55305f625b41fdc7acb65fb2cb3e8c2b0666d5f5
Hifu <artix1995@gmail.com>
2026-08-26 13:18:31 +0200

    Phase 2: isolate bootstrap workflow controllers

--- include/bootstrap_controller_ps2.h
+++ include/bootstrap_controller_ps2.h
@@ -4,28 +4,32 @@
 #include "apa.h"
 #include "boot_chain.h"
 
-void bootstrap_controller_backup_current(
-    unsigned char header[APA_HEADER_SIZE],
-    boot_chain_info_t *boot_chain);
-
-void bootstrap_controller_disable(
-    unsigned char header[APA_HEADER_SIZE],
-    boot_chain_info_t *boot_chain);
-
-void bootstrap_controller_restore(
-    unsigned char header[APA_HEADER_SIZE],
-    boot_chain_info_t *boot_chain);
-
+/* These are human-authorized, storage/MagicGate-dominated workflows. Keep each
+ * controller as its own compact call target so whole-program LTO cannot fold
+ * backup/disable/restore/install into the interactive Bootstrap menu and make
+ * that menu compete with recovery code for the R5900's 16 KiB I-cache. */
 #if defined(__GNUC__)
-#define BOOTSTRAP_INSTALL_SIZE_OPT __attribute__((optimize("Os")))
+#define BOOTSTRAP_WORKFLOW_STAGE __attribute__((noinline, optimize("Os")))
 #else
-#define BOOTSTRAP_INSTALL_SIZE_OPT
+#define BOOTSTRAP_WORKFLOW_STAGE
 #endif
 
-void BOOTSTRAP_INSTALL_SIZE_OPT bootstrap_controller_install(
+void BOOTSTRAP_WORKFLOW_STAGE bootstrap_controller_backup_current(
     unsigned char header[APA_HEADER_SIZE],
     boot_chain_info_t *boot_chain);
 
-#undef BOOTSTRAP_INSTALL_SIZE_OPT
+void BOOTSTRAP_WORKFLOW_STAGE bootstrap_controller_disable(
+    unsigned char header[APA_HEADER_SIZE],
+    boot_chain_info_t *boot_chain);
+
+void BOOTSTRAP_WORKFLOW_STAGE bootstrap_controller_restore(
+    unsigned char header[APA_HEADER_SIZE],
+    boot_chain_info_t *boot_chain);
+
+void BOOTSTRAP_WORKFLOW_STAGE bootstrap_controller_install(
+    unsigned char header[APA_HEADER_SIZE],
+    boot_chain_info_t *boot_chain);
+
+#undef BOOTSTRAP_WORKFLOW_STAGE
 
 #endif
