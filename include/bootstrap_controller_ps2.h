cb47886d9b229cf87421da2758526273078267d3
Hifu <artix1995@gmail.com>
2026-08-26 13:03:25 +0200

    Phase 2: size-optimize bootstrap install controller

--- include/bootstrap_controller_ps2.h
+++ include/bootstrap_controller_ps2.h
@@ -16,8 +16,16 @@ void bootstrap_controller_restore(
     unsigned char header[APA_HEADER_SIZE],
     boot_chain_info_t *boot_chain);
 
-void bootstrap_controller_install(
+#if defined(__GNUC__)
+#define BOOTSTRAP_INSTALL_SIZE_OPT __attribute__((optimize("Os")))
+#else
+#define BOOTSTRAP_INSTALL_SIZE_OPT
+#endif
+
+void BOOTSTRAP_INSTALL_SIZE_OPT bootstrap_controller_install(
     unsigned char header[APA_HEADER_SIZE],
     boot_chain_info_t *boot_chain);
+
+#undef BOOTSTRAP_INSTALL_SIZE_OPT
 
 #endif
